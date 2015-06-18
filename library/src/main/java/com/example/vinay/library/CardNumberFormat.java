96f3eb7391f9fa5f5349d0b2e8a9cc39ce4ca197
vinay <vinaygaba@gmail.com>
2015-06-18 23:22:15 +0530

    -Fixed null pointer bug in type logic -Fixed typo

--- library/src/main/java/com/example/vinay/library/CardNumberFormat.java
+++ library/src/main/java/com/example/vinay/library/CardNumberFormat.java
@@ -7,6 +7,6 @@ public class CardNumberFormat {
 
     public static final int ALL_DIGITS = 0;
     public static final int MASKED_ALL_BUT_LAST_FOUR = 1;
-    public static final int ONLY_LAZST_FOUR = 2;
+    public static final int ONLY_LAST_FOUR = 2;
     public static final int MASKED_ALL = 3;
 }
