54e63597956efd2e0a3b767be397591166138f0a
vinay <vinaygaba@gmail.com>
2015-06-18 12:12:24 +0530

    -Created Constant values for card type and card number format to use programatically

---
+++ library/src/main/java/com/example/vinay/library/CardNumberFormat.java
@@ -0,0 +1,12 @@
+package com.example.vinay.library;
+
+/**
+ * Created by vinay on 6/18/2015.
+ */
+public class CardNumberFormat {
+
+    public static final int ALL_DIGITS = 0;
+    public static final int MASKED_ALL_BUT_LAST_FOUR = 1;
+    public static final int ONLY_LAZST_FOUR = 2;
+    public static final int MASKED_ALL = 3;
+}
---
+++ library/src/main/java/com/example/vinay/library/CardType.java
@@ -0,0 +1,16 @@
+package com.example.vinay.library;
+
+/**
+ * Created by vinay on 6/18/2015.
+ */
+public class CardType {
+    public static final int VISA = 0;
+
+    public static final int MASTERCARD = 1;
+
+    public static final int AMERICAN_EXPRESS = 2;
+
+    public static final int DISCOVER = 3;
+
+    public static final int AUTO = 4;
+}
