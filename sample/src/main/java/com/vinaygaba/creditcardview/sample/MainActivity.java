6b5fb437858b95592975c8c6b4525d3ec439cbdf
Vinay Gaba <vinaygaba@gmail.com>
2018-08-05 11:01:35 -0700

    Revert unintentional changes

--- sample/src/main/java/com/vinaygaba/creditcardview/sample/MainActivity.java
+++ sample/src/main/java/com/vinaygaba/creditcardview/sample/MainActivity.java
@@ -3,8 +3,6 @@ package com.vinaygaba.creditcardview.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.vinaygaba.creditcardview.CreditCardView;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CreditCardView creditCardView = findViewById(R.id.card3);
-        creditCardView.setCardName("Vinay Gaba");
     }
-
 }
